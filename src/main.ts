445e8ce420070869996636e90275cd8bae52dc03
Keisuke Isono <reirou.k@gmail.com>
2023-04-03 00:25:43 +0900

    Add base files

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+// main.ts
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: BrowserWindow | null = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+// Create the mainWindow when Electron has finished initializing
+app.on('ready', createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
